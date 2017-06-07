ccfe4e542cf37a74a9a74d177caf492ccbf5c02f
Kovács Vince <vincekovacs@hotmail.com>
2017-06-07 11:32:06 +0200

    Exporting api

---
+++ index.ts
@@ -0,0 +1,5 @@
+export {Indicator, IndicatorStatus, IndicatorResponse, IndicatorOptions} from "./src/Indicator";
+
+export {IndicatorNotFoundError} from "./src/error/IndicatorNotFoundError";
+
+export {Xray} from "./src/Xray";
